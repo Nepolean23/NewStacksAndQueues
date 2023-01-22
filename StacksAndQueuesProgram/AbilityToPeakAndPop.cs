47c7d239c7d2ac97b45b03ada70313a7990f03e7
Nepolean23 <nlean1996@gmail.com>
2023-01-22 12:48:54 +0530

    [NEPOLEAN]-Code for Performing a peak and pop operation in Linkedlist stack

---
+++ StacksAndQueuesProgram/AbilityToPeakAndPop.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    internal class AbilityToPeakAndPop
+    {
+        private Node top;
+        public AbilityToPeakAndPop()
+        {
+            this.top = null;
+        }
+
+        internal void push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack: ", value);
+        }
+
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+        internal void peek()
+        {
+            if(this.top==null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the stack",this.top.data);
+        }
+
+        internal void pop() 
+        {
+            if(this.top==null)
+            {
+                Console.WriteLine("Stack is not empty,Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("Value popped is {0}",this.top.data);
+            this.top = this.top.next;
+        }
+
+        internal void IsEmpty()
+        {
+            while(this.top!=null)
+            {
+                peek();
+                pop();
+            }
+        }
+    }
+}
+
--- StacksAndQueuesProgram/Program.cs
+++ StacksAndQueuesProgram/Program.cs
@@ -1,9 +1,19 @@
 ﻿using StacksAndQueues;
 
-//Performing of stack operations
-LinkedListStack List=new LinkedListStack();
-List.push(70);
-List.push(30);
-List.push(56);
-List.Display();
+////Performing of stack operations
+//LinkedListStack List=new LinkedListStack();
+//List.push(70);
+//List.push(30);
+//List.push(56);
+//List.Display();
 
+//Performing of peek and pop operations
+AbilityToPeakAndPop AbilityToPeakAndPop=new AbilityToPeakAndPop();
+AbilityToPeakAndPop.push(70);
+AbilityToPeakAndPop.push(30);
+AbilityToPeakAndPop.push(56);
+AbilityToPeakAndPop.Display();
+AbilityToPeakAndPop.peek();
+AbilityToPeakAndPop.pop();
+AbilityToPeakAndPop.IsEmpty();
+AbilityToPeakAndPop.Display();
