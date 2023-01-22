c9c7361070c01406fd2acfafc49599e675857352
Nepolean23 <nlean1996@gmail.com>
2023-01-22 12:23:44 +0530

    [Nepolean]-Code for Performing Push operations in LinkedListStack

--- StacksAndQueuesProgram/Program.cs
+++ StacksAndQueuesProgram/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using StacksAndQueues;
+
+//Performing of stack operations
+LinkedListStack List=new LinkedListStack();
+List.push(70);
+List.push(30);
+List.push(56);
+List.Display();
+
